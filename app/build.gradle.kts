4d5ef587a1c14d316bb0b55d9c6143d96764b919
satendragoswamii <satendragoshwami@gmail.com>
2026-06-12 03:47:32 +0530

    Update to AGP 8.7.3 and Kotlin 2.0.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,9 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.webkit:webkit:1.10.0")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.webkit:webkit:1.12.1")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 }
